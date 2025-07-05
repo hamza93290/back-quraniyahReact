ad8c8005ed14d03b81255a620f547ed5c3c5c56c
hamza <66466122+hamza93290@users.noreply.github.com>
2025-07-05 17:54:09 +0200

    seconde

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ import nodemailer from 'nodemailer';
 
 
 // Initialiser Firebase Admin avec la clé de service
-const serviceAccount = JSON.parse(readFileSync('serviceAccountKey.json'));
+config(); // Charge les variables d’environnement
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
